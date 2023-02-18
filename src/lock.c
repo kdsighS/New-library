2a92b73cf10aca0ed28370dd4e4e615e1a6e5817
kdsighS <kjsdrow@tutanota.com>
2023-02-18 18:48:36 +0800

    Add(develop-log-1rd): Add log functions.

--- src/lock.c
+++ src/lock.c
@@ -1,13 +1,13 @@
 #include "lock.h"
 #include "header.h"
 
-struct lock_t create_lock_t(unsigned int max){
-	struct lock_t lock;
+lock_t create_lock_t(unsigned int max){
+	lock_t lock;
 	lock.max = max;
-	lock.min = 0;
+	lock.now = 0;
 	return lock;
 };
-int try_lock_t(struct lock_t* lock){
+int try_lock_t(lock_t* lock){
 	if (lock -> now < lock -> max){
 		lock -> now++;
 		return NL_OK;
@@ -15,10 +15,12 @@ int try_lock_t(struct lock_t* lock){
 		return NL_ERR;
 	};
 };
-int while_lock_t(struct lock_t* lock){
+int while_lock_t(lock_t* lock){
 	while(lock -> now >= lock ->max);
 	lock -> now++;
+	return NL_OK;
 };
-int un_lock_t(struct lock_t* lock){
+int un_lock_t(lock_t* lock){
 	lock -> now--;
+	return NL_OK;
 };
